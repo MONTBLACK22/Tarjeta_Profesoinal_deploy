1b370f755e9138154fc895268770058332cdab99
MONTBLACK22 <150577516+MONTBLACK22@users.noreply.github.com>
2026-08-05 11:51:29 -0500

    video_button

--- 8thwall/src/playbutton.js
+++ 8thwall/src/playbutton.js
@@ -4,35 +4,45 @@ ecs.registerComponent({
   name: 'PlayPauseButton',
 
   schema: {
-    videoPlane: ecs.eid,
-    textEntity: ecs.eid,
+    button: ecs.eid,      // El botón UI (Play)
+    videoPlane: ecs.eid,  // El Plane que contiene el Video Controls
+    textEntity: ecs.eid,  // El hijo Text del botón
   },
 
   data: {
-    isPlaying: false,
+    playing: ecs.boolean,
   },
 
-  stateMachine: ({world, eid, schemaAttribute}) => {
-    return ecs.defineState('default')
+  stateMachine: ({world, eid, schemaAttribute, dataAttribute}) => {
+    dataAttribute.set(eid, {
+      playing: true, // Como Autoplay está activado
+    })
+
+    ecs
+      .defineState('default')
       .initial()
 
-      .listen(eid, ecs.ui.CLICK, () => {
+      .listen(
+        schemaAttribute.get(eid).button,
+        ecs.input.UI_CLICK,
+        () => {
+          const state = dataAttribute.get(eid)
+          const {videoPlane, textEntity} = schemaAttribute.get(eid)
 
-        const data = ecs.PlayPauseButton.get(world, eid)
-        const {videoPlane, textEntity} = schemaAttribute.get(eid)
+          dataAttribute.mutate(eid, (cursor) => {
+            cursor.playing = !cursor.playing
+          })
 
-        data.isPlaying = !data.isPlaying
+          ecs.VideoControls.mutate(world, videoPlane, (video) => {
+            video.paused = !state.playing
+            return false
+          })
 
-        ecs.PlayPauseButton.set(world, eid, data)
-
-        ecs.VideoControls.mutate(world, videoPlane, (video) => {
-          video.paused = !data.isPlaying
-          return false
-        })
-
-        ecs.UiText.set(world, textEntity, {
-          text: data.isPlaying ? 'Pause' : 'Play'
-        })
-      })
-  }
+          ecs.Ui.mutate(world, textEntity, (ui) => {
+            ui.text = state.playing ? 'Play' : 'Pause'
+            return false
+          })
+        }
+      )
+  },
 })
