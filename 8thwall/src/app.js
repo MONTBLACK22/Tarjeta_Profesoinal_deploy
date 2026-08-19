33b80bd041bb316c1f599238d13564e0922f1c65
MONTBLACK22 <150577516+MONTBLACK22@users.noreply.github.com>
2026-08-18 23:42:19 -0500

    fix: Configurar imageTargetData en app.js para tarjeta_profesional_tobon-1

--- 8thwall/src/app.js
+++ 8thwall/src/app.js
@@ -1,7 +1,9 @@
 const onxrloaded = () => {
   XR8.XrController.configure({
     imageTargetData: [
-      require('../image-targets/targetimage.json')
+      require('../image-targets/tarjeta_profesional_tobon-1.json'),
+      require('../image-targets/tarjeta_profesional_tobon.json'),
+      require('../image-targets/targetimage.json'),
     ],
   })
 }
