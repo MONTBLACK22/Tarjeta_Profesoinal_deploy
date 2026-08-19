f0fe380df5d34d776f253845e435a6f23efbc4d8
MONTBLACK22 <150577516+MONTBLACK22@users.noreply.github.com>
2026-08-19 00:11:20 -0500

    build: Recompilar bundle con nuevas rotaciones y tarjeta_profesional_tobon-2

--- 8thwall/src/app.js
+++ 8thwall/src/app.js
@@ -1,6 +1,7 @@
 const onxrloaded = () => {
   XR8.XrController.configure({
     imageTargetData: [
+      require('../image-targets/tarjeta_profesional_tobon-2.json'),
       require('../image-targets/tarjeta_profesional_tobon-1.json'),
       require('../image-targets/tarjeta_profesional_tobon.json'),
       require('../image-targets/targetimage.json'),
